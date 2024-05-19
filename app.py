b91e6eebca6cee342de71cdd4aa1df51db2880dc
Bel-93 <84254342+Bel-93@users.noreply.github.com>
2024-05-19 01:13:01 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+
+    import streamlit as st
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Título de la aplicación
+    st.title('App de Ejemplo para Google Colab')
+
+    # Dataframe de ejemplo
+    df = pd.DataFrame({
+        'x': np.random.randn(100),
+        'y': np.random.randn(100)
+    })
+
+    # Gráfico de dispersión
+    st.write("Gráfico de Dispersión:")
+    st.write(df)
+    st.write(plt.scatter(df['x'], df['y']))
+    
